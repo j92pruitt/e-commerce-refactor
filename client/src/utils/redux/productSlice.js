68806a5923063be3a7b358a6084d11680e4593ab
j92pruitt <j92pruitt@gmail.com>
2021-10-16 18:27:14 -0700

    Added store to the react app and corrected some mistakes in productSlice

--- client/src/utils/redux/productSlice.js
+++ client/src/utils/redux/productSlice.js
@@ -1,20 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+    products: [],
+}
+
 export const productSlice = createSlice({
     name: 'product',
 
-    initialState: {
-        products: [],
-    },
+    initialState,
 
     reducers: {
 
         UPDATE_PRODUCTS: (state, action) => {
-            state.products = action.products
+            state.products = action.payload
         }
     }
 })
 
 export const { UPDATE_PRODUCTS } = productSlice.actions;
+
+export const selectProducts = (state) => state.product.products
 
 export default productSlice.reducer
