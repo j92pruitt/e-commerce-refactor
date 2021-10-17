07611cc1e707970b91da59cff3baa2aa60b5d0ef
j92pruitt <j92pruitt@gmail.com>
2021-10-17 12:51:12 -0700

    Removed unneed import

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
 import { idbPromise } from "../../utils/helpers";
 
 import { useDispatch } from 'react-redux';
